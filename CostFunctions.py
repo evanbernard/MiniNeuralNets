fixed backpropagation

CostFunctions.py
@@ -1,11 +1,5 @@
 import numpy as np
 
 
-# sigmoid(x, deriv) takes in a number (x) and boolean (deriv), and will return the result of the sigmoid function
-#   applied to x if deriv is False, and it will return the result of the derivative of the sigmoid function
-#   otherwise
-def sigmoid(x, deriv=False):
-    if deriv:
-        return np.exp(-x) / (np.exp(-x) + 1) ** 2
-    else:
-        return 1 / (np.exp(-x) + 1)
+def sigmoid(x):
+    return 1 / (np.exp(-x) + 1)

Perceptron.py
@@ -1,5 +1,4 @@
-import numpy as np
-import CostFunctions
+from CostFunctions import *
 
 # The structure of this neural net is as follows, where I represents an input node, N a neuron and O the output.
 # I -
@@ -7,9 +6,10 @@ import CostFunctions
 # I -- N - O
 #     /
 # I -
+# The neural net is trained via backpropagation
 
 
-def train(t_inputs, t_outputs, num_iterations=10000):
+def train(t_inputs, t_outputs, num_iterations=1000):
     num_inputs = len(t_inputs[0])
     # we want a vector of length n, where the values are between -1 and 1
     weights = np.random.rand(1, num_inputs) * 2 - 1
@@ -21,16 +21,16 @@ def train(t_inputs, t_outputs, num_iterations=10000):
         for i in range(len(t_inputs)):
             input_layer = t_inputs[i]
             neuron_val = np.dot(input_layer, weights.T)
-            t_output = CostFunctions.sigmoid(neuron_val)
-            outputs = np.append(outputs, t_output)
+            output = sigmoid(neuron_val)
+            outputs = np.append(outputs, output)
 
-        t_errors = t_outputs.T - outputs
+        t_errors = (t_outputs.T - outputs) * (outputs * (1 - outputs))
+
         if iteration == num_iterations - 1:
             accuracy = 1 - np.mean(abs(t_errors))
             print("ACCURACY: \n {}".format(accuracy))
-
-        # the magnitude of an adjustment is directly proportional to the error and the derivative of the cost function
-        adjustments = np.dot(t_errors * CostFunctions.sigmoid(outputs, True), t_inputs)
+        # the magnitude of an adjustment is directly proportional to the error
+        adjustments = np.dot(t_errors, t_inputs)
         weights = weights + adjustments
     return weights
 
@@ -39,7 +39,7 @@ def train(t_inputs, t_outputs, num_iterations=10000):
 #   prediction as output
 def predict(weights, p_input):
     neuron_val = np.dot(p_input, weights.T)
-    p_output = CostFunctions.sigmoid(neuron_val)
+    p_output = sigmoid(neuron_val)
     return p_output[0]
 
 
@@ -48,14 +48,15 @@ if __name__ == "__main__":
                                 [0, 0, 1, 0],
                                 [1, 0, 1, 0],
                                 [0, 0, 0, 1],
-                                [1, 0, 1, 1]])
+                                [1, 0, 1, 1],
+                                [0, 1, 1, 1]])
 
-    training_outputs = np.array([0, 0, 0, 1, 1])
+    training_outputs = np.array([0, 0, 0, 1, 1, 1])
 
     # notice this input is unique from the inputs trained
-    input_vals = [0, 0, 0, 1]
+    input_vals = [0, 1, 1, 0]
     output = predict(train(training_inputs, training_outputs, num_iterations=10000), input_vals)
     prediction = round(output)
-    error = abs(prediction - output)
+    error = (prediction - output)**2
     print("INPUT \n {}".format(input_vals))
-    print("PREDICTION \t\t ERROR \n {} \t\t\t\t {}".format(prediction, error))
+    print("PREDICTION \t\t ERROR \n {} \t\t\t {}".format(prediction, error))
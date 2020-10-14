150ce3c86d2836301277fbda189de43bb8ba6f87
Evan Bernard <evanbernardd@gmail.com>
2020-10-14 13:39:20 -0400

    backprop better comments and error function

--- Backpropagation.py
+++ Backpropagation.py
@@ -1,7 +1,23 @@
 from CostFunctions import *
 
 
-def backpropagation(x, y, num_iterations=1000, activation=sigmoid):
+def backpropagation(x, y, iterations=1000, activation=sigmoid, error_func=difference):
+    """
+    SUMMARY
+        The function's purpose is to generate weights that are most likely to generate a label in y, given a row of
+        inputs (each row in x). It begins by randomly generating weights for the model, and it adjusts the weights by
+        a factor of the derivative of the activation function and the error, so for each iteration, the weights are
+        adjusted to be slightly more accurate than the previous iteration.
+    PARAMETERS
+        x: a numpy array, one row for each trial (set of inputs)
+        y: a numpy array, labels for the inputs x. one label per trial
+        iterations: integer, number of times we will adjust the weights of the model
+        activation: function, the type of activation function to be used. (sigmoid is the best for this model)
+        error_func: function, the type of function to calculate the error. (difference is the best for this model)
+    RETURN
+        The function returns two elements, the numpy array of best weights found, as well as the accuracy of the
+        weights, in that order.
+    """
     num_inputs = len(x[0])
     accuracy = 0
     # we are adding a bias by creating a new node (val=1) in the previous layer and treating it as an input, so append
@@ -10,29 +26,30 @@ def backpropagation(x, y, num_iterations=1000, activation=sigmoid):
     temp = []
     for row in range(len(x)):
         temp.append(np.append(x[row], 1))
-    t_inputs = np.array(temp)
+    x = np.array(temp)
 
     # we want a vector of length n + 1 (one extra for a bias) for weights, where the values are between -1 and 1
     weights = np.random.rand(1, num_inputs + 1) * 2 - 1
 
     # In each iteration, we run the model on every testing input and output, and adjust the weights once
-    for iteration in range(num_iterations):
-        outputs = np.array([])
+    for iteration in range(iterations):
+        y_hats = np.array([])
         neuron_vals = np.array([])
 
-        for i in range(len(t_inputs)):
-            input_layer = t_inputs[i]
+        for i in range(len(x)):
+            input_layer = x[i]
             neuron_val = np.dot(weights, input_layer)
             y_hat = activation(neuron_val)
-            outputs = np.append(outputs, y_hat)
+            y_hats = np.append(y_hats, y_hat)
             neuron_vals = np.append(neuron_vals, neuron_val)
 
-        t_errors = (y - outputs) * activation(neuron_vals, deriv=True)
+        t_errors = error_func(y, y_hats) * activation(neuron_vals, deriv=True)
 
-        if iteration == num_iterations - 1:
+        if iteration == iterations - 1:
             accuracy = 1 - np.mean(abs(t_errors))
 
         # the magnitude of an adjustment is directly proportional to the error
-        adjustments = np.dot(t_errors, t_inputs)
+        adjustments = np.dot(t_errors, x)
+
         weights = weights + adjustments
     return weights, accuracy
